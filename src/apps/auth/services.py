b902b7f1b1104a283de2d82c60cb8b86f8b4ce69
Ivan <iivan.zybin@gmail.com>
2025-05-17 14:03:39 +0300

    fix

--- src/apps/auth/services.py
+++ src/apps/auth/services.py
@@ -139,8 +139,8 @@ class InviteService:
 
         return invite_id
 
-    async def _make_invite_link(self, register_url, invite_id) -> str:
-        return f"{register_url}?invite_id={invite_id}"
+    async def _make_invite_link(self, register_url, invite_id, type) -> str:
+        return f"{register_url}{invite_id}"
 
     async def _send_email_invite(self, invite_schema: InviteSchema, invite_id: UUID) -> None:
         invite_link = self._make_invite_link(invite_schema.register_url, invite_id)
